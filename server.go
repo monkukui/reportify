4a580ea22382003155eb607ae1048b9417b3e693
jlwt90 <jasonwaiting@live.hk>
2023-03-05 23:11:50 +0900

    finished sample server

---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"github.com/99designs/gqlgen/graphql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/jlwt90/gqlgen-usage-analysis/graph"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	c := graph.Config{Resolvers: &graph.Resolver{}}
+	c.Directives.HasRole = func(ctx context.Context, obj interface{}, next graphql.Resolver, id string) (res interface{}, err error) {
+		return next(ctx)
+	}
+	c.Directives.Lang = func(ctx context.Context, obj interface{}, next graphql.Resolver, region string) (res interface{}, err error) {
+		return next(ctx)
+	}
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(c))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
